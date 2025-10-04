cd68e796b76236d6fc528a1ed9da1ebf790d24a2
Tomi <ahmadbustomi1507@gmail.com>
2025-10-04 18:53:04 +0700

    structured the files

--- test/android/astralife-go/ava-iPro-Terjamin/createContract.js
+++ test/android/astralife-go/ava-iPro-Terjamin/createContract.js
@@ -1,33 +1,58 @@
+
 const { expect, browser, driver } = require('@wdio/globals');
+
+const { randomNumber,randomFullName } = require('../../../resources/generate.data.js');
+const submissionPage = require('../../../pages/astralife-go/submissions/iPro/submission.page.js');
 const createNewCustomerPage = require('../../../pages/contract/createNewCustomer.page.js');
 const saveRiskProfilePage = require('../../../pages/contract/saveRiskProfile.page.js');
 const saveQuotationPage = require('../../../pages/contract/saveQuotation.page.js');
 const saveProposalPage = require('../../../pages/contract/saveProposal.page.js');
 const saveESubmissionPage = require('../../../pages/contract/saveESubmission.page.js');
-const { randomNumber } = require('../../../resources/generate.data.js');
-const { randomFullName } = require('../../../resources/generate.data.js');
+
+
 // Mengambil data dari satu file JSON
-const data = require('../../../test/ios/ava_infinite_protection/data/AIP_P_data.json');
+const data = require('../../../test/android/astralife-go/ava-iPro-Terjamin/data/Polis-1-monthly.json');
+// const h = get data from files 
 
-describe('New Contract Test Case', () => {
+// async function getFilesFromFolder() {
+//   // Request access to a directory
+//   const directoryHandle = await window.showDirectoryPicker();
+//   const files = [];
 
+//   // Iterate through the directory entries
+//   for await (const entry of directoryHandle.values()) {
+//     if (entry.kind === 'file') {
+//       files.push(entry.name); // Add file names to the list
+//     }
+//   }
+
+//   console.log(files); // Output the list of files
+// }
+
+getFilesFromFolder();
+
+describe('Create new contract for AVA iPro Terjamin', () => {
+    // h.array.forEach((polis) => {
+    //   asdsf
+    // });
     // Extract data dari file JSON
-    const customerData = data.customerData;
-    const correspondentData = data.correspondentData;
-    const addressData = data.addressData;
-    const factaWNIData = data.factaWNIData;
-    const otherData = data.otherData;
-    const quotationData = data.quotationData;
-    const paymentDetailsData = data.paymentDetailsData;
-    const declarationData = data.declarationData;
-    const declarationForm = data.declarationForm;
-    const riskProfileData = data.riskProfileData;
+    // const customerData = data.customerData;
+    // const correspondentData = data.correspondentData;
+    // const addressData = data.addressData;
+    // const factaWNIData = data.factaWNIData;
+    // const otherData = data.otherData;
+    // const quotationData = data.quotationData;
+    // const paymentDetailsData = data.paymentDetailsData;
+    // const declarationData = data.declarationData;
+    // const declarationForm = data.declarationForm;
+    // const riskProfileData = data.riskProfileData;
 
 
-    it('Flow Positive Submit Contract - AVA Infinite Protection ', async () => {
+    it('Create polis static', async () => {
+
+      
       await createNewCustomerPage.fillCustomerInformation(customerData);
       await saveRiskProfilePage.clickNewQuotationButton();
-      
       await saveRiskProfilePage.clickProductTypeButton(riskProfileData.productType);
       await saveRiskProfilePage.clickCustInfoButton();
       await saveRiskProfilePage.verifyCustomerInfo(customerData);
@@ -49,39 +74,30 @@ describe('New Contract Test Case', () => {
       });
 
 
+      // 1. Screen Product
+      // Step 1.1 konfirmasi kesehatan
+      // Step 1.2 konfirmasi cetak
+      // Step 1.3 pengisian premium dan hitung sum assured
+
+      // 2. Screen Package
+      // Step 3.1 konfirmasi benefit
+
+      // 3. Screen identitas diri
+      // Step 3.1 isi nama dan ktp
+      // Step 3.2 isi identitas diri
+      // step 3.3 isi beneficiary 
+
+      // 4. Screen Konfirmasi
+      // Step 4.1 tanda tangan SPPN  
+      // Step 4.2 tanda tangan SPAJ 
+      // Step 4.3 tanda tangan SKPR/SKB 
+      // Step 4.4 tanda tangan Riplay Personal
+
+      // 5. Screen Pembayaran
+      //Do nothing here 
+
+
+
     });
     
-
-    // it('Flow From Existing Data', async () => {
-    //   await saveRiskProfilePage.clickExistingButton();
-    //   await createNewCustomerPage.searchCustomerName('Ritsuki D Automation');
-    //   await saveRiskProfilePage.clickExistingQuotationButton();
-      
-    //   await saveESubmissionPage.customButtonByID('Declaration').waitForDisplayed({timeout: 10000});
-    //   await saveESubmissionPage.customButtonByID('Declaration').click();
-    //   await driver.pause(25000);
-    //   await saveESubmissionPage.customButtonByID('Next >').click();
-    //   await saveESubmissionPage.customButtonByID('Next >').click();
-    //   // await saveESubmissionPage.customButtonByID('e-Submission >').waitForDisplayed({timeout: 8000});
-    //   // await saveESubmissionPage.customButtonByID('e-Submission >').click();
-    //   // await saveESubmissionPage.customButtonByID('Confirm').waitForDisplayed();
-    //   // await saveESubmissionPage.customButtonByID('Confirm').click();
-    //   // await saveESubmissionPage.customButtonByID('Teruskan').waitForDisplayed({timeout: 4000});
-    //   // await saveESubmissionPage.customButtonByID('Teruskan').click();
-
-    //   // await saveESubmissionPage.customButtonByID('Payment >').click();
-    //   // await saveESubmissionPage.customButtonByID('Amendment >').click();
-    //   // await saveESubmissionPage.customButtonByID('No, proceed to eSubmission').waitForDisplayed({timeout: 3000});
-    //   // await saveESubmissionPage.customButtonByID('No, proceed to eSubmission').click();
-
-    //   await saveProposalPage.fillDeclarationForm(declarationForm);
-    //   // await saveESubmissionPage.addSignature({
-    //   //   name: 'Park Jisunga Automation',
-    //   // });
-
-
-    // });
-
-
-   
 });
